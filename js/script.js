2bba33a3e88778ce2bd906ce609d0ba179c2e291
flan the dev <flanthedev@gmail.com>
2017-08-24 00:55:44 +0800

    add custom font

--- js/script.js
+++ js/script.js
@@ -1,10 +1,18 @@
-(function() {
-	let containerElement = document.getElementById('container');
+let containerElement = document.getElementById('container');
 
+(function() {
 	for (i = 0; i < 9; i++) {
 		let box = document.createElement('div');
 		box.id = `box${i}`;
 		box.className = 'box';
+		box.innerText = 'X';
 		containerElement.appendChild(box);
 	}
 })();
+
+containerElement.addEventListener('click', () => {
+	let box = event.target;
+	box.textContent = 'X';
+
+
+});
