49461b9c26d3c14af88f16959942435fba76b7da
flan the dev <flanthedev@gmail.com>
2017-08-24 21:19:30 +0800

    change boxes array to true/false values for easy comparison

--- js/script.js
+++ js/script.js
@@ -11,11 +11,11 @@ const game = {
 		messageElement.textContent = message1;
 		for (i = 0; i < 9; i++) {
 			let box = document.createElement('div');
+			box.textContent = ' ';
+			game.boxes.push(box.textContent);
 			box.id = `box${i}`;
 			box.className = 'box';
 			box.addEventListener('click', game.handler, false);
-			game.boxes.push(box);
-			console.log(game.boxes);
 			containerElement.appendChild(box);
 		}
 	},
@@ -26,18 +26,41 @@ const game = {
 
 	handler: function() {
 		let box = event.target;
+		let boxNumber = event.target.id[3];
 		if (game.isEven(game.turn)) {
 			box.textContent = 'X';
+			game.boxes[boxNumber] = true;
 			game.turn++;
 			messageElement.textContent = message2;
 		} else {
 			box.textContent = 'O';
+			game.boxes[boxNumber] = false;
 			game.turn++;
 			messageElement.textContent = message1;
 		}
 		box.removeEventListener('click', game.handler, false);
+		game.checkWin();
 	},
 
+	checkWin: function() {
+			if (game.boxes[0] === game.boxes[1] === game.boxes[2]) {
+				console.log('win');
+			}
+
+	}
 
 };
+
 game.render();
+
+// possible win combos:
+// 0 1 2
+// 3 4 5
+// 6 7 8
+
+// 0 3 6
+// 1 4 7
+// 2 5 8
+
+// 0 4 8
+// 2 4 8 
