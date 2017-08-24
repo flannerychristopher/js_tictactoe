45c64a4fa77dac20d8d26584969f6d106d3d9b1e
flan the dev <flanthedev@gmail.com>
2017-08-24 19:38:35 +0800

    move turn, isEven method and box array into game object

--- js/script.js
+++ js/script.js
@@ -2,15 +2,10 @@ const containerElement = document.getElementById('container');
 const messageElement = document.getElementById('message');
 const message1 = "Player 1's turn!";
 const message2 = "Player 2's turn!";
-let turn = 0;
-
-function isEven(num) {
-	if (num % 2 === 0) {
-		return true;
-	}
-}
 
 const game = {
+	turn: 0,
+	boxes: [],
 
 	render: function() {
 		messageElement.textContent = message1;
@@ -19,28 +14,29 @@ const game = {
 			box.id = `box${i}`;
 			box.className = 'box';
 			box.addEventListener('click', game.handler, false);
+			game.boxes.push(box);
+			console.log(game.boxes);
 			containerElement.appendChild(box);
 		}
 	},
 
+	isEven: function(num) {
+		if (num % 2 === 0) return true;
+	},
+
 	handler: function() {
 		let box = event.target;
-		if (isEven(turn)) {
+		if (game.isEven(game.turn)) {
 			box.textContent = 'X';
-			turn++;
+			game.turn++;
 			messageElement.textContent = message2;
 		} else {
 			box.textContent = 'O';
-			turn++;
+			game.turn++;
 			messageElement.textContent = message1;
 		}
 		box.removeEventListener('click', game.handler, false);
 	},
-
-	test: function() {
-		console.log('handler');
-	}
-
 
 
 };
